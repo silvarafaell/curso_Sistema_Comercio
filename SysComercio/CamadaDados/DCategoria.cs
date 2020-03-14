923ddfdb9d28fc8ece34d0db095f824d0a77aba0
francisco rafael <francisco_rafael@hotmail.com.br>
2020-03-14 12:04:37 -0300

    criando classe de conexao categorias

---
+++ SysComercio/CamadaDados/DCategoria.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CamadaDados
+{
+    class DCategoria
+    {
+        private int _Idcategoria;
+        private string _Nome;
+        private string _Descricao;
+        private string _TextoBuscar;
+
+        public int Idcategoria 
+        { 
+            get
+            {
+                return Idcategoria;
+            }
+            set
+            {
+                Idcategoria = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return Nome;
+            }
+            set
+            {
+                Nome = value;
+            }
+        }
+
+        public string Descricao
+        {
+            get
+            {
+                return Descricao;
+            }
+            set
+            {
+                Descricao = value;
+            }
+        }
+
+        public string TextoBuscar
+        {
+            get
+            {
+                return Descricao;
+            }
+            set
+            {
+                Descricao = value;
+            }
+        }
+    }
+}
